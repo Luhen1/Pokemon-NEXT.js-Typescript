538ef2af8365698d3e310e0e5e7d6120eeb406ea
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2022-01-02 21:58:38 -0300

    fixing ssr

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,10 @@ import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
 import { useState } from "react";
 
+const btn =
+  "inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
+
 export default function Home() {
   
   const [ids, updateIds] = useState(() => getOptionsForVote());
@@ -13,19 +17,28 @@ export default function Home() {
 
   if (firstPokemon.isLoading ||secondPokemon.isLoading) return null;
 
+  const voteForRoundest = (selected: number) => {
+    //todo: fire mutation to persist changes 
+
+    updateIds(getOptionsForVote());
+
+  }
+
   return  (
   <div className="h-screen w-screen flex flex-col justify-center items-center">
     <div className="text-2xl text-center">Which Pokemon is rounder?</div>
     <div className="p-2" />
     <div className="border p-8 flex justify-between items-center max-w-2xl">
-      <div className="w-64 h-64 flex flex-col">
+      <div className="w-64 h-64 flex flex-col items-center">
         <img src={firstPokemon.data?.sprites.front_default}  className = "w-full" />
         <div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
+        <button className={btn} onClick={() => voteForRoundest(first)}>Rounder</button>
         </div>
       <div className="p-8">Vs</div>
-      <div className="w-64 h-64 flex flex-col">
+      <div className="w-64 h-64 flex flex-col items-center">
         <img src={secondPokemon.data?.sprites.front_default} className = "w-full" />
         <div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
+        <button className={btn} onClick={() => voteForRoundest(second)}>Rounder</button>
         </div>
         <div className="p-2" />
       </div>
